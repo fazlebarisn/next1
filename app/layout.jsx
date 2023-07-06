24188354bed772ae1d168103f1177dde737d12c8
Fazle Bari <fazlebarisn@gmail.com>
2023-07-06 16:35:18 +0600

    Update layout

---
+++ app/layout.jsx
@@ -0,0 +1,13 @@
+import '@styles/globals.css';
+export const metadata = {
+    titile: "CodeAstrology",
+    description: "Web development company",
+}
+
+const layout = () => {
+  return (
+    <div>layout</div>
+  )
+}
+
+export default layout
